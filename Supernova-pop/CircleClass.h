698fb45b9bff329ca1ff8b564c7e442b78a84b09
Jonathan Mayhak <jmayhak@gmail.com>
2013-08-24 03:56:02 -0400

    restarted w/o gravity

---
+++ Supernova-pop/CircleClass.h
@@ -0,0 +1,28 @@
+//
+//  CircleClass.h
+//  Supernova-pop
+//
+//  Created by Jonathan Mayhak on 8/23/13.
+//  Copyright 2013 Jason Rikard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface CircleClass : CCLayer {
+    float _size_of_circle;
+    float _x_location;
+    float _y_location;
+    float _correct_band;
+    float _rate_of_growth;
+    bool _is_visible;
+    bool _is_locked;
+    CCParticleSystemQuad *sunFire;
+}
+
+-(bool) isVisible;
+-(bool) isLocked;
+
++(CCScene *) scene;
+
+@end
---
+++ Supernova-pop/HelloWorldLayer.h
@@ -0,0 +1,26 @@
+//
+//  HelloWorldLayer.h
+//  Supernova-pop
+//
+//  Created by Jason Rikard on 8/23/13.
+//  Copyright Jason Rikard 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+    CGFloat gameTime;
+    CCArray *_circles;
+    int _num_circles_at_a_time;
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
