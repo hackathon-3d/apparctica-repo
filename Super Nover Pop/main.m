a8d2893b1ebf4a6dec739f9c69a47bef04b4eca7
Jason Rikard <jsnrkd@gmail.com>
2013-08-23 20:52:14 -0400

    Initial Commit

---
+++ Super Nover Pop/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Super Nover Pop
+//
+//  Created by Jason Rikard on 8/23/13.
+//  Copyright Jason Rikard 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
